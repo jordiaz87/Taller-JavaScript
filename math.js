3e7a129a338247aaa68848595fbab6059f503863
Jorge Diaz <jorjorf@gmail.com>
2022-10-03 15:55:09 -0400

    Reto: altura de un triángulo

--- math.js
+++ math.js
@@ -20,60 +20,69 @@
 
 // console.groupEnd('Cuadrado');
 
-// console.group('Triangulo');
+console.group('Triangulo');
 
-// const ladoTriangulo1 = 6; 
-// const ladoTriangulo2 = 6; 
-// const ladoTrianguloBase = 4; 
-// const alturaTriangulo = 5.5;
+const ladoTriangulo1 = 6; 
+const ladoTriangulo2 = 6; 
+const ladoTrianguloBase = 4; 
+const alturaTriangulo = 5.5;
 
-// const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
-// const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2;
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
+const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2;
 
 
-// function calcularTriangulo(lado1, lado2, base, altura) {
-//     return {
-//         perimetro: lado1 + lado2 + base,
-//         area: (base * altura) / 2,
-//     }
-// }
-
-// console.log({
-//     ladoTriangulo1,
-//     ladoTriangulo2,
-//     ladoTrianguloBase,
-//     alturaTriangulo,
-//     perimetroTriangulo,
-//     areaTriangulo,
-// })
-
-// console.groupEnd('Triangulo');
-
-console.group('Circulo');
-
-const radioCirculo = 3;
-const diametroCirculo = radioCirculo * 2; 
-const PI = 3.1415;
-
-const circunferencia = diametroCirculo * PI;
-const areaCirculo = (radioCirculo ** 2) * PI;
-
-console.log({
-    radioCirculo,
-    diametroCirculo,
-    PI,
-    circunferencia,
-    areaCirculo,
-})
-
-function calcularCirculo(radio){
-
-    const diametro = radio * 2;
-    const radioAlCuadrado = Math.pow(radio, 2);
-    return{
-        circunferencia: diametro * Math.PI,
-        area: radioAlCuadrado * Math.PI,
+function calcularTriangulo(lado1, lado2, base, altura) {
+    return {
+        perimetro: lado1 + lado2 + base,
+        area: (base * altura) / 2,
     }
 }
 
-console.groupEnd('Circulo');
+function calcularAlturaTriangulo(lado1, base) {
+    if (lado1 == base) {
+        console.warn('Este no es un triangulo isosceles');
+    } else {
+        // h = raizcuadrada(lado1**2 - (b**2)/4)
+        return Math.sqrt( (lado1 ** 2) - ( (base ** 2) ) / 4 );
+    }
+}
+
+console.log({
+    ladoTriangulo1,
+    ladoTriangulo2,
+    ladoTrianguloBase,
+    alturaTriangulo,
+    perimetroTriangulo,
+    areaTriangulo,
+})
+
+console.groupEnd('Triangulo');
+
+// console.group('Circulo');
+
+// const radioCirculo = 3;
+// const diametroCirculo = radioCirculo * 2; 
+// const PI = 3.1415;
+
+// const circunferencia = diametroCirculo * PI;
+// const areaCirculo = (radioCirculo ** 2) * PI;
+
+// console.log({
+//     radioCirculo,
+//     diametroCirculo,
+//     PI,
+//     circunferencia,
+//     areaCirculo,
+// })
+
+// function calcularCirculo(radio){
+
+//     const diametro = radio * 2;
+//     const radioAlCuadrado = Math.pow(radio, 2);
+//     return{
+//         circunferencia: diametro * Math.PI,
+//         area: radioAlCuadrado * Math.PI,
+//     }
+// }
+
+// console.groupEnd('Circulo');
