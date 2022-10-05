232fee943880ca95e34313b7074bb7abb1644cdd
Jorge Diaz <jorjorf@gmail.com>
2022-10-05 13:57:29 -0400

    Calculando el promedio

---
+++ platzimath.js
@@ -0,0 +1,12 @@
+function calcularPromedio(lista) {
+
+    let sumaLista = 0;
+
+    for (let i=0; i < lista.length; i++) {
+        sumaLista = sumaLista +  lista[i];
+    }
+
+    const promedio = sumaLista / lista.length;
+    console.log(promedio);
+    return promedio
+}
