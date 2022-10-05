6af7937030aa444c36bc074aab89548a52cb1826
Jorge Diaz <jorjorf@gmail.com>
2022-10-05 16:39:40 -0400

    Calculando la mediana en una lista impar

--- platzimath.js
+++ platzimath.js
@@ -1,14 +1,29 @@
+function esPar (lista) {
+    return !(lista.length % 2);
+}
+
+// otra opcion
+// function esImpar (lista) {
+//     return lista.length % 2;
+// }
+
+function calcularMediana(lista) {
+    const listaEsPar = esPar(lista);
+
+    if (listaEsPar) {
+
+    } else {
+        const indexMitadListaImpar = Math.floor(lista.length / 2);
+        const medianaListaImpar = lista[indexMitadListaImpar];
+        console.log(indexMitadListaImpar)
+        console.log(medianaListaImpar);
+        return medianaListaImpar;
+    }
+}
+
+
 function calcularPromedio(lista) {
 
-    // let sumaLista = 0;
-
-    // for (let i=0; i < lista.length; i++) {
-    //     sumaLista = sumaLista +  lista[i];
-    // }
-
-    // function sumarTodosElementos(valorAcumulado, nuevoValor) {
-    //     return valorAcumulado + nuevoValor;
-    // }
 
 
     const sumarTodosElementos = (valorAcumulado, nuevoValor) => valorAcumulado + nuevoValor;
