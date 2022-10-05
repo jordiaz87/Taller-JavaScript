374dbbe20ceb3b9197ba911559389dcc79349ce8
Jorge Diaz <jorjorf@gmail.com>
2022-10-05 14:54:25 -0400

    Método reduce

--- platzimath.js
+++ platzimath.js
@@ -1,12 +1,24 @@
 function calcularPromedio(lista) {
 
-    let sumaLista = 0;
+    // let sumaLista = 0;
 
-    for (let i=0; i < lista.length; i++) {
-        sumaLista = sumaLista +  lista[i];
-    }
+    // for (let i=0; i < lista.length; i++) {
+    //     sumaLista = sumaLista +  lista[i];
+    // }
+
+    // function sumarTodosElementos(valorAcumulado, nuevoValor) {
+    //     return valorAcumulado + nuevoValor;
+    // }
+
+
+    const sumarTodosElementos = (valorAcumulado, nuevoValor) => valorAcumulado + nuevoValor;
+    
+    const sumaLista = lista.reduce(sumarTodosElementos);
+
+
 
     const promedio = sumaLista / lista.length;
+
     console.log(promedio);
     return promedio
 }
