c84c69a2fdbb6f74f42a27912bca2a8ae62ba96e
Jorge Diaz <jorjorf@gmail.com>
2022-10-04 22:17:45 -0400

    Método find vs. método filter

--- descuentos.js
+++ descuentos.js
@@ -5,6 +5,32 @@ const pResult = document.querySelector('#result')
 
 btn.addEventListener('click', calcularPrecioConDescuento);
 
+// const cuponObj = {
+//     'ElMejor': 30,
+//     '23232':25,
+//     '123': 15,
+// };
+
+const cuponsList = [];
+cuponsList.push({
+    name: 'elmejor',
+    discount: 30,
+}); 
+
+
+cuponsList.push({
+    name: 'elmasutil',
+    discount: 25,
+}); 
+
+
+cuponsList.push({
+    name: 'erestu',
+    discount: 15,
+}); 
+
+
+
 function calcularPrecioConDescuento() {
     // (P*(100-D))/100
 
@@ -18,20 +44,49 @@ function calcularPrecioConDescuento() {
 
     let discount;
 
-    switch (cupon) {
-        case 'elmejor':
-            discount = 30;
-            break;
+    function isCuponInArray(cuponElement) {
+        return cuponElement.name == cupon;
 
-        case 'elmasretador':
-            discount = 25;
-            break;
-    
-        default:
-            pResult.innerHTML= 'El cupon no es valido';
-            return;
-            break;
     }
+
+    const cuponInArray = cuponsList.find(isCuponInArray);
+
+
+    if (cuponInArray) {
+        discount = cuponInArray.discount;
+    } else {
+        pResult.innerHTML= 'El cupon no es valido';
+        return;
+    }
+
+    console.log({
+        cupon,
+        discount,
+        cuponInArray,
+        cuponsList
+    })
+
+    // if (cuponObj[cupon]) {
+    //     discount = cuponObj[cupon];
+    // } else {
+    //     pResult.innerHTML= 'El cupon no es valido';
+    //     return;
+    // }
+
+    // switch (cupon) {
+    //     case 'elmejor':
+    //         discount = 30;
+    //         break;
+
+    //     case 'elmasretador':
+    //         discount = 25;
+    //         break;
+    
+    //     default:
+    //         pResult.innerHTML= 'El cupon no es valido';
+    //         return;
+    //         break;
+    // }
 
     // if (cupon == 'elmejor') {
     //     discount = 30;
