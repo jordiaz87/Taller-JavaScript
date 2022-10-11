6516f8d84447cba20ee60554c1ea7ae08015013d
Jorge Diaz <jorjorf@gmail.com>
2022-10-10 20:36:38 -0400

    Salarios empresariales

--- analisis.js
+++ analisis.js
@@ -60,5 +60,33 @@ function proyeccionPorPersona(nombrePersona) {
 
 }
 
+// analisis empresarial
+
+const empresas = {};
+for ( persona of salarios ) {
+    for ( trabajo of persona.trabajos) {
+        if ( !empresas[trabajo.empresa]) {
+            empresas[trabajo.empresa] = {};
+        }
+
+        if (!empresas[trabajo.empresa][trabajo.year]) {
+            empresas[trabajo.empresa][trabajo.year] = [];
+        }
+
+        empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+    }
+}
+
+console.log(empresas);
 
 
+function medianaEmpresaYear(nombre, year) {
+    if (!empresas[nombre]) {
+        console.warn('La empresa no existe');
+    } else if (!empresas[nombre][year]) {
+        console.warn('La empresa no dio salarios ese año');
+    } else {
+        return PlatziMath.calcularMediana(empresas[nombre][year]);
+    }
+
+}
